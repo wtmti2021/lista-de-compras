ad37ead0eb8ad1924d5dba07e398672f7476dd3f
Willian Teixeira <93533318+wtmti2021@users.noreply.github.com>
2022-08-14 10:41:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+  let lista = document.getElementById('produto')
+  let botao = document.getElementById('btn')
+
+  botao.addEventListener('click', function() {
+    let item = produto.value++
+    console.log(item)
+  })
+  
+  
+  
+  
